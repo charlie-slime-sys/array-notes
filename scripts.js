b8a072cd12d45c30ab48bab54253165fb24469cf
charlie-slime-sys <sawyer.pulsipher24@student.cart.org>
2025-02-13 14:34:42 -0800

    Update scripts.js with more notes

--- scripts.js
+++ scripts.js
@@ -38,9 +38,69 @@ console.log(hobbies);
 
 let listItem = hobbies[0];
 console.log(listItem);
-let listLast = hobbies[2];
+let listLast = hobbies[hobbies.length-1];
 console.log(listLast);
 
 let seasons = ['Winter', 'Spring', 'Summer', 'Fall']
 seasons[3]= 'Autumn';
 console.log(seasons);
+
+// variables declared with let can be reassigned, but variables with const, it cannot. 
+//elements in an array declared with const remain mutable.
+// ^ meaning, we can change the contents of a const array, but not reassign a new array.
+
+// practice underneath
+
+let condiments = ['Ketchup', 'Mustard', 'Relish'];
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+const utensils = ['fork', 'spoon', 'knife'];
+utensils[2] = 'spork';
+console.log(utensils);
+
+// arrays built in property is length, and it returns the number of items in an array. We access .length like we do with strings
+
+const newYearsResolution = ['Keep a journal', 'Take a falconry class'];
+console.log(newYearsResolution.length);
+console.log(newYearsResolution[newYearsResolution.length -1])
+// >> Output: 2
+
+// These built in method call to arrays to make common tasks possible
+// one method, .push() allows to add items to the end of an array
+
+const itemTracker = ['orange', 'apple', 'kiwi'];
+itemTracker.push('apricot', 'mango');
+console.log(itemTracker);
+// >> Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+// we use the push method by dot notation, connecting push  to itemTracker with a .
+// Then, we call it like a function, because .push() is a function that js allows to use on an array
+// .push() can take a single argument or multiple arguments separated by a column
+// notice that .push() changes or mutates itemTracker
+// you can also see .push() referred to as a destructive method since it changes the og array
+// if looking for a method that will mutate an array by adding elements t o it, then .push() is the method for you :)
+
+// guh, practice
+
+const chores = ['actually cleaning'];
+chores.push('do art', 'play epic video games', 'make food to eat');
+console.log(chores);
+
+const remove = chores.pop();
+console.log(remove);
+
+const newItemTracker = ['guh 0', 'guh 1', 'guh 2'];
+const removed = newItemTracker.pop();
+console.log(newItemTracker);
+// >> Output: [ 'item 0', 'item 1' ]
+console.log(removed);
+// >> Output: item 2
+
+//.pop() does not take any arguments whatsoever
+// returns the value of the last element
+// also a method of mutation, and also destructive.
+// use .pop() to remove the last element if you need to mutate an array that way.
+
+/* sigh,
+throw monokuma and watches him as he explodes */
